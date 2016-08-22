3334514845edd86e60341397edb544b25afc8dd9
Arnaud Grosjean <arnaudg@MOMENTFACTORY.LOCAL>
2016-08-22 17:42:49 -0400

    now working with a JSON input

--- mavlink/mavlink.js
+++ mavlink/mavlink.js
@@ -4,28 +4,10 @@ module.exports = function(RED) {
   function MavlinkNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-
     var myMAV = new mavlink(1,1,"v1.0",["common","ardupilotmega"]);
 
     this.on('input', function(msg) {
-      command = {
-          name:"COMMAND_LONG",
-          parameters:{
-          'target_system' : 1,
-          'target_component' : 0,
-          'command' : 22,
-          'confirmation' : 1,
-          'param1': 1,
-          'param2': "",
-          'param3': "",
-          'param4': 0,
-          'param5': "",
-          'param6': "",
-          'param7': 40
-           }
-      }
-
-      myMAV.createMessage(command.name, command.parameters,
+      myMAV.createMessage(msg.payload.parameters, msg.payload.parameters,
         function(mavMessage) {
           msg.payload = mavMessage.buffer;
           node.send(msg);
